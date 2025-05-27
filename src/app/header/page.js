ff12a17856a0eee9b2f2717e96bfcbe5f6630e20
shobanbalaji <shobanbalaji.v@gmail.com>
2025-05-27 16:24:33 +0530

    fix

--- src/app/header/page.js
+++ src/app/header/page.js
@@ -3,11 +3,13 @@ import React, { useState, useEffect, useRef} from "react";
 import { Col, Row } from "react-bootstrap";
 import Link from "next/link";
 import { GiHamburgerMenu } from "react-icons/gi";
+import { useRouter } from "next/router";
 
 
 const Header = ({isBackground=true, menuColor = "white"}) => {
   const [menuOpen, setMenuOpen] = useState(false);
   const menuRef = useRef()
+  const nav = useRouter()
 
   const handleShowMenu = () => {
     setMenuOpen(!menuOpen);
@@ -51,7 +53,7 @@ const Header = ({isBackground=true, menuColor = "white"}) => {
         <div className="d-flex justify-content-between align-items-center pe-3"> 
           <h3
             className="py-5 ps-5 header-logo-text"
-            onClick={() => (window.location.href = "/")}
+            onClick={() => (nav.push('/'))}
             style={{
               fontSize: "35px",
               fontWeight: "bold",
