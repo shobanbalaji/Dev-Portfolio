9eec838081e20812f006ad20b1a43788017f7126
shobanbalaji <shobanbalaji.v@gmail.com>
2025-07-12 19:28:57 +0530

    footer update

--- src/app/footer/page.js
+++ src/app/footer/page.js
@@ -2,6 +2,11 @@ import React from "react";
 import Link from "next/link";
 import { Row, Col } from "react-bootstrap";
 const Footer = () => {
+
+  const handleVoice = ()=>{
+    console.log("render")
+  }
+  
   return (
     <footer className="dev-footer">
       <div className="footer-container px-5 pt-5 pb-3">
@@ -9,27 +14,30 @@ const Footer = () => {
 
         <Row className="justify-content-start footer-content">
           <Col md={4}>
-            <h4> shobanbalaji.v@gmail.com</h4>
+            <h4 typeof="email"> shobanbalaji.v@gmail.com</h4>
             <h4> t.me/shoban</h4>
           </Col>
           <Col md={4}>
-            <h4> <Link href="/article" style={{textDecoration:"none", color:"inherit"}}> My shelf </Link></h4>
+            <h4> <Link href="/blog" style={{textDecoration:"none", color:"inherit"}}> My shelf </Link></h4>
             <h4> <Link href="/project" style={{textDecoration:"none", color:"inherit"}}> My Works </Link></h4>
             <h4> <Link href="/resume" style={{textDecoration:"none", color:"inherit"}}> My Resume </Link></h4>
           </Col>
         </Row>
 
 
-        <hr className="my-5 footer-strike"/>
+        <hr className="my-3 footer-strike"/>
 
         <Row className="footer-final justify-content-between">
           <Col> <h4> Copyrights shbnmllx</h4> </Col>
           <Col>
           <Row className="justify-content-end"> 
-            <Col> <span style={{cursor:"pointer", fontSize:"15px"}}>TW </span> </Col>
-            <Col> <span style={{cursor:"pointer", fontSize:"15px"}}>IG </span> </Col>
-            <Col> <span style={{cursor:"pointer", fontSize:"15px"}}>LN </span> </Col>
+            <Col md={1}> <Link href="/blog" style={{cursor:"pointer", fontSize:"15px", textDecoration:"none"}}>TW </Link> </Col>
+            <Col md={1}> <Link href="/blog" style={{cursor:"pointer", fontSize:"15px", textDecoration:"none"}}>TW </Link> </Col>
+            <Col md={1}> <Link href="/blog" style={{cursor:"pointer", fontSize:"15px", textDecoration:"none"}}>IG </Link> </Col>
+            <Col md={1}> <Link href="/blog" style={{cursor:"pointer", fontSize:"15px", textDecoration:"none"}}>LN </Link> </Col>
           </Row>
+
+            <h1 onClick={handleVoice}> seasam</h1>
 
           </Col>
         </Row>
